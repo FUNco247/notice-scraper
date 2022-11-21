181829219869d57f00a53899f943f92478e25da8
jinwook <crispy.alc@gmail.com>
2022-11-21 15:57:06 +0900

    feat: get SBA with selenium

--- SBA.py
+++ SBA.py
@@ -1,13 +1,25 @@
-import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
 
 SBA_URL : str = "https://www.sba.seoul.kr/Pages/ContentsMenu/Company_Support.aspx?C=6FA70790-6677-EC11-80E8-9418827691E2"
 
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
 def getSBAPage(url : str):
-    req = requests.get(url)
-    text = req.text
-    soup = BeautifulSoup(text,"html.parser")
-    table = soup.find("div",{"id":"container"}).find("div",{"class":"content"}).find("div",{"id":"card_list"}).findChildren()
-    print(table)
+    driver.get(url)
+    time.sleep(1.5)
+    html = driver.page_source
+    soup = BeautifulSoup(html,"html.parser")
+    table = soup.find("div",{"id":"card_list"})
+    cards = table.find_all("div",{"class":"card_box"})
+    result = []
+    for card in cards:
+        title = card.find("a").text
+        date = card.find("dd",{"class":"date"}).text
+        info = {"title" : title, "date":date}
+        result.append(info)
+    print(result)
 
 getSBAPage(SBA_URL)
